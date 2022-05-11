d21eeb18fb93f2bb2b531c77cf0d2e5587505981
liuk <kekeliu_job@163.com>
2022-05-11 11:13:20 +0800

    vue基础

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-03-22 10:47:18
+ * @LastEditTime: 2022-03-26 10:20:53
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /Vue/README.md
+-->
 # Vue
+## Vue、React、Angular 都有自己的脚手架, 它们的脚手架都集成了 webpack 这样的打包工具。所以框架的话用脚手架就自带构建工具了, 无需再下载构建工具。
+### Vue CLI: 是一个用于快速 Vue.js 开发的完整系统。
+### Vue的编程设计模式应该叫做 mvvm 的设计模式。它首先是面向数据的编程，程序中定义了数据，然后定义了模板，Vue就可以把数据和模板自动进行关联。最后挂载到真实的DOM上，展示给用户。第一个m代表model数据，第一个v代表view视图，最后两个字母vm代表viewModel视图数据连接层。
---
+++ vue-project/src/App.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-03-23 10:51:53
+ * @LastEditTime: 2022-03-26 20:04:14
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /Vue/vue-project/src/App.vue
+-->
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
