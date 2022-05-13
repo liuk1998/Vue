3a7adeb83b417ab2f0671b2f9ca26760e355906f
liuk <kekeliu_job@163.com>
2022-05-13 16:14:42 +0800

    组件基础

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,25 +1,29 @@
 <!--
  * @Author: your name
  * @Date: 2022-03-23 10:51:53
- * @LastEditTime: 2022-03-26 20:04:14
- * @LastEditors: your name
+ * @LastEditTime: 2022-05-13 16:10:54
+ * @LastEditors: liuk kekeliu_job@163.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /Vue/vue-project/src/App.vue
 -->
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <GetName></GetName>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// 子组件引入
+import HelloWorld from './components/HelloWorld.vue';
+import GetName from './components/GetName.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, // 子组件引入
+    GetName
   }
 }
 </script>
---
+++ vue-project/src/components/GetName.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: liuk kekeliu_job@163.com
+ * @Date: 2022-05-13 16:08:15
+ * @LastEditors: liuk kekeliu_job@163.com
+ * @LastEditTime: 2022-05-13 16:09:53
+ * @FilePath: /Vue/vue-project/src/components/GetName.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "刘可",
+    };
+  },
+};
+</script>
